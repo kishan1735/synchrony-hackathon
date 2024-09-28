587f0e86838fcb28e999cb5a8c5a2f3ea9b249ae
kishan1735 <gswwarriorsnba@gmail.com>
2024-09-28 13:46:23 +0530

    feat: god level stuff

---
+++ app/visualise/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import AuthCheck from "@/components/AuthCheck";
+import NavBar from "@/components/NavBar";
+import TransactionGraph from "@/components/TransactionGraph";
+import { Block } from "@/lib/chain";
+
+export default function Visualise() {
+  const chain: Block[] = [
+    { sender: "Alice", receiver: "Bob", amount: 50, timestamp: Date.now() },
+    { sender: "Bob", receiver: "Charlie", amount: 30, timestamp: Date.now() },
+    { sender: "Charlie", receiver: "Dave", amount: 20, timestamp: Date.now() },
+    { sender: "Dave", receiver: "Alice", amount: 25, timestamp: Date.now() },
+  ];
+  return (
+    <AuthCheck>
+      <div className="flex flex-col min-h-screen text-slate-200 gap-12 items-center">
+        <NavBar />
+        <TransactionGraph chain={chain} />
+      </div>
+    </AuthCheck>
+  );
+}
